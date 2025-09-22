1d2bf21874fe18f50a48e3b6fe6ecda0955cd166
Urimash312 <davigabrielnos@Gmail.com>
2025-09-22 16:48:52 -0300

    sim

--- js/Ativ.js
+++ js/Ativ.js
@@ -2,7 +2,7 @@ var y=parseInt(Math.random(0, 100)*100)
 var a
 alert("tente adivinhar o numero que estou pensando de 0 a 100")
 a=prompt("Digite um numero")
-while(a!=y){
+while(a != y){
     if(a<y){
         alert("é maior")
     }
